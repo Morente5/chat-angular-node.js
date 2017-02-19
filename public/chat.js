50b4f415bdb0989fbb9a6427ac864556c0e801d8
Alejandro Morente <morente5@hotmail.es>
2017-02-19 11:11:34 +0100

    Fixed port for Heroku to listen, renamed some events

--- public/chat.js
+++ public/chat.js
@@ -3,11 +3,11 @@ var socket = io();
 $('form').submit( function(event) {
 	event.preventDefault();
   var msg = $('#msg').val();
-	socket.emit('my event', msg);	
+	socket.emit('message', msg);	
 	$('#msg').val('');
 } );
 
-socket.on('my event', function(msg) {
+socket.on('message', function(msg) {
   console.log('msg');
   $('#messages').append($(`<p>${msg}</p>`));
 });
--- server/index.js
+++ server/index.js
@@ -3,16 +3,24 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.use(express.static('client'))
+app.use(express.static('public'));
 
-
-server.listen(3333, function() {
-	console.log('Welcome')
-});
 
 io.on('connection', function(socket) {
-	socket.on( 'my event', function(data) {
-		io.sockets.emit('my event', data);
-		console.log(data)
-	} );
+	console.log('Somebody connected');
+	socket.on('message', function(msg) {
+		console.log(msg);
+		io.sockets.emit('message', msg);
+	});
+
+	socket.on('disconnect', function() {
+		console.log('Somebody disconnected')
+	});
+});
+
+const PORT = process.env.PORT || '3333';
+
+server.listen(PORT, function() {
+	const port = process.env.PORT || '3333';
+	console.log(`Listening on port ${PORT}`);
 });
